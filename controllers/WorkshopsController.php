922ecad21c8711a9e674c7b8d6c557fe5be5ba24
Malak Essam <malakroshdy2006@gmail.com>
2026-04-27 01:56:01 +0300

    Update WorkshopsController.php

--- controllers/WorkshopsController.php
+++ controllers/WorkshopsController.php
@@ -1,8 +1,12 @@
 <?php
 include "../../config/db_connection.php";
 
-$sql = "SELECT * FROM workshop";
+$sql = "
+SELECT workshop.*, speaker.name AS speaker_name
+FROM workshop
+JOIN speaker
+ON workshop.speaker_id = speaker.id
+";
 
-$results = mysqli_query($connection, $sql);
-
+$results = mysqli_query($connection,$sql);
 ?>
