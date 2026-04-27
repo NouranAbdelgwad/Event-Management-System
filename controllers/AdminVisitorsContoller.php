4f27372c93f7a762f8458dcafebde3bdafc9a418
saraalbashir94-png <saraalbashir94@gmail.com>
2026-04-27 17:53:01 +0300

    update admin visitors

---
+++ controllers/AdminVisitorsContoller.php
@@ -0,0 +1,12 @@
+<?php
+require_once "../config/db_connection.php"; 
+
+if (isset($_GET['delete_id'])) {
+    $id = intval($_GET['delete_id']); 
+    $sql = "DELETE FROM user WHERE id = $id";
+    if (mysqli_query($conn, $sql)) {
+        header("Location: ../views/pages/admin_visitors.php?status=success");
+        exit();
+    }
+}
+?>
